e21fd36eeafe5b3669b1f0111326216c31144c78
MarioValiente2 <120366933+MarioValiente2@users.noreply.github.com>
2024-01-10 19:52:19 +0100

    Delete Ej00 directory

--- Ej00/ConAtributo/Tester.java
+++
@@ -1,8 +0,0 @@
-package Metodos.Ej00.ConAtributo;
-
-public class Tester {
-    public static void main(String[] args) {
-        OperacionesArray numero=new OperacionesArray();
-        System.out.println(numero.toString());
-    }
-}
